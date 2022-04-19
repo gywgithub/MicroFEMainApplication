32372d837ede3168b8c223b7f4b9ccf16f9c561a
gywgithub <qingyi_w@outlook.com>
2022-04-19 17:19:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# MicroFEMainApplication
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+or
+```
+yarn dev
+```
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    Micro FE Main Application
+    <img src="./assets/logo.png" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
